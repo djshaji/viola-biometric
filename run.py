46b82095ecf644cea55da69a058a708a18be6c9d
Shaji Khan <quotablecode@gmail.com>
2023-05-25 16:51:56 +0530

    web

--- run.py
+++ run.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python
 print("Content-Type: text/html")
 print()
+import cgi
+args = []
 import cgitb
 cgitb.enable()
+form = cgi.FieldStorage()
 
 print ('<nav class="navbar bg-primary">   <div class="container justify-content-center d-flex">     <a class="text-white navbar-brand" href="#">       <img src="/logo.png" width="72" >&nbsp;saक्षam</a>   </div> </nav>')
+#print (f"<div class='alert alert-success'>{form}</div>")
 
 print ("""
-<form action='/run.py' class='section row m-4 p-4 shadow'>
-    <div class='col-8'>
-        <input placeholder='Enter image URL' name='url' type='text' class='form-control' id='basic-url' aria-describedby='basic-addon3'>
+<form enctype='multipart/form-data' action='/run.py' method='post' class='section row m-4 p-4 shadow'>
+    <div class='col-6'>
+        <input placeholder='Enter image URL' name='url' type='text' class='d-none form-control' id='basic-url' aria-describedby='basic-addon3'>
+        <div class="input-group mb-3">
+          <input name="image" type="file" class="form-control" id="inputGroupFile02">
+          <label class="input-group-text" for="inputGroupFile02">Upload</label>
+        </div>
     </div>
     <div class='col-4'>
-      <select class='form-select' name='f>
+      <select class='form-select' name='f'>
         <option value='demo/faces'>Demo</option>
         <option>Students</option>
         <option>Teachers</option>
         <option>All</option>
       </select>
     </div>
+    <button type='submit' class='p-2 col-2 btn btn-primary'><i class="fas fa-fingerprint"></i>&nbsp;Detect</button>
 </form>
 """)
 
-print ('<div class="section m-3 p-2 shadow">\
+print ('<div class="section m-4 p-4 shadow">\
 <div class="accordion" id="accordionExample">\
   <div class="accordion-item">\
     <h2 class="accordion-header" id="headingOne">\
@@ -33,29 +42,35 @@ print ('<div class="section m-3 p-2 shadow">\
     <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">\
       <div class="accordion-body">')
 
-import cgi
-args = cgi.parse()
 
-print ('<script src="util.js"></script>    <script src="https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js"></script> <script src="https://www.gstatic.com/firebasejs/9.1.3/firebase-auth-compat.js"></script> <script src="https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics-compat.js"></script> <script src="https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js"></script> <script src="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.js"></script> <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script> <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script> <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> <link rel="shortcut icon" type="image/jpg" href="anneli/assets/img/favicon.png"/> <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"> <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script> <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> <link type="text/css" rel="stylesheet" href="/anneli/themer_mdl.php?font=Montserrat&theme=blue_deep_orange&skin=materia"> <link type="text/css" rel="stylesheet" href="/anneli/themer2.php?font=Montserrat&theme=blue_deep_orange&skin=materia"> <link type="text/css" rel="stylesheet" href="anneli/assets/css/all.min.css"> <link type="text/css" rel="stylesheet" href="anneli/assets/css/style.css"> <link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.css" /> <script src="/firebaseConfig.js?1684940450"></script> <script src="anneli/util.js?1684940450"></script> <script src="anneli/mime.js"></script> <script src="anneli/colors.js?"></script> <script src="anneli/fonts.js?"></script> ')
-print (args)
+print ('<script src="https://www.gstatic.com/firebasejs/9.1.3/firebase-app-compat.js"></script> <script src="https://www.gstatic.com/firebasejs/9.1.3/firebase-auth-compat.js"></script> <script src="https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics-compat.js"></script> <script src="https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js"></script> <script src="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.js"></script> <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script> <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script> <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> <link rel="shortcut icon" type="image/jpg" href="anneli/assets/img/favicon.png"/> <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"> <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script> <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> <link type="text/css" rel="stylesheet" href="/anneli/themer_mdl.php?font=Montserrat&theme=blue_deep_orange&skin=materia"> <link type="text/css" rel="stylesheet" href="/anneli/themer2.php?font=Montserrat&theme=blue_deep_orange&skin=materia"> <link type="text/css" rel="stylesheet" href="anneli/assets/css/all.min.css"> <link type="text/css" rel="stylesheet" href="anneli/assets/css/style.css"> <link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/3.5.2/firebaseui.css" /> <script src="/firebaseConfig.js?1684940450"></script> <script src="anneli/util.js?1684940450"></script> <script src="anneli/mime.js"></script> <script src="anneli/colors.js?"></script> <script src="anneli/fonts.js?"></script> ')
 import sys, os
 from deepface import DeepFace
 from PIL import Image, ImageDraw, ImageFont
 import urllib.request
-if args is not None:
+image = None
+folder = None
+if "image" not in form:
+    args = cgi.parse()
+if "i" in args:
     image = args ["i"][0]
+if "f" in args:
     folder = args ["f"][0]
-form = cgi.FieldStorage()
+
 if form.getfirst ("url") is not None:
     image = form.getfirst ("url")
-
+if "image" in form:
+    fn = os.path.basename(form ["image"].filename)
+    open('tmp/' + fn, 'wb').write(form ["image"].file.read())
+    image = 'tmp/' + fn
 if form.getfirst ("f") is not None:
     folder = form.getfirst ("f")
-if image.startswith ("http"):
-    urllib.request.urlretrieve(image, "/var/www/viola/tmp/" + os.path.basename (image))
-    image = "tmp/" + os.path.basename (image)
+
+# if image.startswith ("http"):
+    # urllib.request.urlretrieve(image, "/var/www/viola/tmp/" + os.path.basename (image))
+    # image = "tmp/" + os.path.basename (image)
        
-print (image, folder)
+print ("<div class='alert alert-warning'>",image, folder,"</div>")
 def drawFaces (img, x, y, w, h):
     with Image.open(img) as im:
         draw = ImageDraw.Draw(im)
@@ -63,8 +78,15 @@ def drawFaces (img, x, y, w, h):
         im.save(sys.argv [1] + "-detect", "JPG")
 #faces = DeepFace.extract_faces (sys.argv [1])
 #print (faces)
-d = DeepFace.find (image, folder)
-all_faces = DeepFace.extract_faces (image)
+
+if len (sys.argv) > 2:
+    image = sys.argv [1]
+    folder = sys.argv [2]
+
+if (image is None or folder is None):
+    exit ()
+d = DeepFace.find (image, folder, enforce_detection=False)
+all_faces = DeepFace.extract_faces (image, enforce_detection=False)
 
 im = Image.open(image)
 draw = ImageDraw.Draw(im)
